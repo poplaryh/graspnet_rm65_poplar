eb5a201ec770379c16025df189e972afb825761e
unknown <haoyang1306@qq.com>
2025-05-22 14:27:02 +0800

    okk

--- master_slave.py
+++ master_slave.py
@@ -2,11 +2,11 @@ import time
 import threading
 import os
 import numpy as np
-import queue
 from Robotic_Arm import *
 from Robotic_Arm.rm_robot_interface import *
 import pyrealsense2 as rs
 import cv2
+import datetime
 
 master_trajectory = []
 master_time = []
@@ -16,7 +16,10 @@ save_images = []
 image_time = []
 last_sent_joint = None
 MIN_MOVEMENT_THRESHOLD = 0.05  # 最小运动阈值(弧度)
-zhouqi = 50
+zhouqi = 10
+lock = threading.Lock()
+master_queue = None
+slave_queue = None
 
 pipeline = rs.pipeline()
 config = rs.config()
@@ -24,20 +27,18 @@ config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 60)  # RGB流
 profile = pipeline.start(config)
 frames = pipeline.wait_for_frames()
 
-slave_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
-handle_slave = slave_arm.rm_create_robot_arm("192.168.110.118", 8080)
-master_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
-handle_master = master_arm.rm_create_robot_arm("192.168.110.119", 8080)
-
-def master_state_func(data):
-    global master_trajectory, master_time
-    a = data.joint_status.to_dict()
-    b = time.time()
-    master_trajectory.append(a.copy())
-    master_time.append(b.copy())
+def master_state_func(data1):
+    global master_trajectory, master_time, lock
+    print('主臂也有数据')
+    a1 = data1.joint_status.to_dict()
+    # b = time.time()
+    b1 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    master_trajectory.append(a1.copy())
+    master_time.append(b1)
     
 def slave_state_func(data):
-    global slave_trajectory, slave_time, save_images, image_time, pipeline
+    global slave_trajectory, slave_time, save_images, image_time, pipeline, lock
+    print('有数据')
     a = data.joint_status.to_dict()
     frames = pipeline.wait_for_frames(timeout_ms=1000)
     if not frames:
@@ -45,44 +46,74 @@ def slave_state_func(data):
     else:
         color_frame = frames.get_color_frame()
         color_image = np.asanyarray(color_frame.get_data())
-        b = time.time()
+        # b = time.time()
+        b = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
         slave_trajectory.append(a.copy())
-        slave_time.append(b.copy())
+        slave_time.append(b)
         save_images.append(color_image.copy())
-        image_time.append(b.copy())
+        image_time.append(b)
 
-def master_collect():
-    global zhouqi, master_arm, handle_master, stop_event
 
-    # master_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
-    # handle_master = master_arm.rm_create_robot_arm("192.168.110.119", 8080)
+def master_collect(stop_event):
+    
+    global zhouqi, master_queue
+
+    try:
+        master_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
+        handle_master = master_arm.rm_create_robot_arm("192.168.110.119", 8080)
+    except Exception as e:
+        print(f"创建主臂失败: {e}")
+        return
+    print("创建主臂成功")
 
     custom = rm_udp_custom_config_t()
     custom.joint_speed = 1
     custom.lift_state = 0
     custom.expand_state = 0
     custom.arm_current_status = 1
-    config = rm_realtime_push_config_t(zhouqi, True, 8089, 0, "192.168.110.55", custom)
+    config = rm_realtime_push_config_t(zhouqi, True, 8090, 0, "192.168.110.55", custom)
     print(master_arm.rm_set_realtime_push(config))
     
     arm_state_callback_master = rm_realtime_arm_state_callback_ptr(master_state_func)
     master_arm.rm_realtime_arm_state_call_back(arm_state_callback_master)
+    print('start master cycle')
 
     while not stop_event.is_set():
-        time.sleep(0.1)
+        start_time = time.time()
+        arm_status = master_arm.rm_get_current_arm_state()
+        if arm_status[0] != 0:
+            print("主臂未处于就绪状态，尝试复位...")
+            master_arm.rm_movej([0, 0, 0, 0, 0, 0], 20, 0, 0, 1)
+            continue
+        else:
+            master_queue = arm_status[1]['joint'].copy()
+        
+        # 计算已用时间
+        elapsed_time = time.time() - start_time
+        # 动态调整等待时间，确保每次循环大约0.1秒
+        sleep_time = max(0, 0.005 - elapsed_time)
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+        
     
     if stop_event.is_set():
-        config_end = rm_realtime_push_config_t(zhouqi, False, 8089, 0, "192.168.110.55", custom)
+        config_end = rm_realtime_push_config_t(zhouqi, False, 8090, 0, "192.168.110.55", custom)
         master_arm.rm_set_realtime_push(config_end)
+        print('master arm reset', master_arm.rm_movej([0, 0, 0, 0, 0, 0], 20, 0, 0, 1))
         master_arm.rm_delete_robot_arm()
         print("停止主臂数据采集")
 
 
-def slave_collect():
-    global pipeline, zhouqi, slave_arm, handle_slave, stop_event
+def slave_collect(stop_event):
+    global pipeline, zhouqi, MIN_MOVEMENT_THRESHOLD, slave_queue, master_queue
 
-    # slave_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
-    # handle_slave = slave_arm.rm_create_robot_arm("192.168.110.118", 8080)
+    try:
+        slave_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
+        handle_slave = slave_arm.rm_create_robot_arm("192.168.110.118", 8080)
+    except Exception as e:
+        print(f"创建从臂失败: {e}")
+        return
+    print("创建从臂成功")
 
     custom = rm_udp_custom_config_t()
     custom.joint_speed = 1
@@ -91,93 +122,59 @@ def slave_collect():
     custom.arm_current_status = 1
     config = rm_realtime_push_config_t(zhouqi, True, 8089, 0, "192.168.110.55", custom)
     print(slave_arm.rm_set_realtime_push(config))
-    # print(arm2.rm_get_realtime_push())
 
     arm_state_callback_slave = rm_realtime_arm_state_callback_ptr(slave_state_func)
     slave_arm.rm_realtime_arm_state_call_back(arm_state_callback_slave)
+    print('start slave cycle')
 
     while not stop_event.is_set():
-        time.sleep(0.1)
-    
-    if stop_event.is_set():
-        config_end = rm_realtime_push_config_t(zhouqi, False, 8089, 0, "192.168.110.55", custom)
-        slave_arm.rm_set_realtime_push(config_end)
-        slave_arm.rm_delete_robot_arm()
-        # pipeline.stop()
-        print("停止从臂数据采集")
-
-def master_slave_control():
-
-    global last_sent_joint, master_arm, slave_arm, MIN_MOVEMENT_THRESHOLD, stop_event
-    print("启动主从臂实时控制...")
-    
-    # 确保从臂处于正常状态
-    slave_status = slave_arm.rm_get_current_arm_state()
-    if slave_status[0] != 0:
-        print("从臂未处于就绪状态，尝试复位...")
-        slave_arm.rm_reset_error()
-
-    master_status = master_arm.rm_get_current_arm_state()
-    if master_status[0] != 0:
-        print("主臂未处于就绪状态，尝试复位...")
-        master_arm.rm_reset_error()    
-
-    while not stop_event.is_set():
-        # 记录循环开始时间
         start_time = time.time()
-        
-        # 获取主臂关节位置
-        master_status = master_arm.rm_get_current_arm_state()
-        master_joints = master_status[1]['joint']
-        
-        if master_joints:
+        salve_status = slave_arm.rm_get_current_arm_state()
+        if salve_status[0] != 0:
+            print("从臂未处于就绪状态，尝试复位...")
+            slave_arm.rm_movej([0, 0, 0, 0, 0, 0], 20, 0, 0, 1)
+            continue
+        slave_queue = salve_status[1]['joint'].copy()
+        if master_queue is not None and np.linalg.norm(np.array(master_queue) - np.array(slave_queue)) > MIN_MOVEMENT_THRESHOLD:
+            result = slave_arm.rm_movej_follow(master_queue)
+            print(f"从臂移动到主臂位置: {master_queue}")
             
-            # 只有当移动距离超过阈值时才发送命令
-            if last_sent_joint is None or np.linalg.norm(np.array(master_joints) - np.array(last_sent_joint)) > MIN_MOVEMENT_THRESHOLD:
+            if result != 0:
+                print(f"移动指令失败，错误码: {result}，尝试复位...")
 
-                # 直接使用主臂关节位置控制从臂
-                result = slave_arm.rm_movej_follow(master_joints)
-                
-                if result != 0:
-                    print(f"移动指令失败，错误码: {result}，尝试复位...")
-                    slave_arm.rm_reset_error()
-                else:
-                    last_sent_joint = master_joints.copy()
-            else:
-                print("变化量小于阈值，跳过")
-        else:
-            print('无法获取主臂数据') 
-        
-        # 计算已用时间
         elapsed_time = time.time() - start_time
         # 动态调整等待时间，确保每次循环大约0.1秒
         sleep_time = max(0, 0.005 - elapsed_time)
         if sleep_time > 0:
             time.sleep(sleep_time)
-
-    master_arm.rm_delete_robot_arm()
-    slave_arm.rm_delete_robot_arm()
-                
+    
+    if stop_event.is_set():
+        config_end = rm_realtime_push_config_t(zhouqi, False, 8089, 0, "192.168.110.55", custom)
+        slave_arm.rm_set_realtime_push(config_end)
+        print('salve arm reset', slave_arm.rm_movej([0, 0, 0, 0, 0, 0], 20, 0, 0, 1))
+        slave_arm.rm_delete_robot_arm()
+        print("停止从臂数据采集")              
 
 if __name__ == "__main__":
-    threading.Lock()
     stop_event = threading.Event()
     try:
+        # 把实例传递给线程
         master_collect_thread = threading.Thread(target=master_collect, args=(stop_event, ))
         slave_collect_thread = threading.Thread(target=slave_collect, args=(stop_event, ))
-        master_slave_thread = threading.Thread(target=master_slave_control, args=(stop_event, ))
         master_collect_thread.start()
         slave_collect_thread.start()
-        master_slave_thread.start()
+        # master_slave_thread.start()
+
+        while True:
+            # 这里可以添加其他逻辑，比如检查线程状态等
+            time.sleep(1)
 
     except KeyboardInterrupt:
         print('检测到键盘中断，停止数据采集和控制...')
-    
     finally:
         stop_event.set()
         master_collect_thread.join()
         slave_collect_thread.join()
-        master_slave_thread.join()
         pipeline.stop()
 
         master_pos = 'master_pos.csv'
@@ -212,4 +209,6 @@ if __name__ == "__main__":
             image_path = os.path.join(folder_path, f"{t}.png")
             cv2.imwrite(image_path, img)
 
-
+        print('data length:', len(master_trajectory), len(slave_trajectory), len(save_images))
+        print('total time:', '\n', master_time[-1], master_time[0], '\n', slave_time[-1], slave_time[0], '\n', image_time[-1], image_time[0])
+        print("程序结束")
--- master_slave_arm.py
+++ master_slave_arm.py
@@ -1,118 +1,224 @@
 import time
-import threading
 import os
 import numpy as np
+import datetime
 from Robotic_Arm import *
 from Robotic_Arm.rm_robot_interface import *
+import pyrealsense2 as rs
+import cv2
+from tqdm import tqdm
 
-# 系统配置
-MASTER_IP = '192.168.110.119'  # 主臂IP
-SLAVE_IP = '192.168.110.118'   # 从臂IP
-TCP_PORT = 8080
-MIN_MOVEMENT_THRESHOLD = 0.05  # 最小运动阈值(弧度)
-last_sent_joint = None  # 上次发送的关节位置
+# master_trajectory = []
+# master_time = []
+slave_trajectory = []
+slave_time = []
+save_images1 = []
+save_images2 = []
+image_time = []
 
-# 初始化主臂和从臂
-master_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
-slave_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
-master_handle = master_arm.rm_create_robot_arm(MASTER_IP, TCP_PORT)
-slave_handle = slave_arm.rm_create_robot_arm(SLAVE_IP, TCP_PORT)
-
-file_name = '123.txt'
-data_path = '/home/arm_hao/bottleo/graspnet-baseline'
-file_path_test = os.path.join(data_path, file_name)
-# 确保目录存在
-os.makedirs(os.path.dirname(file_path_test), exist_ok=True)
-
-# 确保文件存在，否则创建空文件
-if not os.path.exists(file_path_test):
-    with open(file_path_test, 'w', encoding='utf-8') as f:
-        pass
-
-save_trajectory = []
-
-# 全局锁
-lock = threading.Lock()
-
-def get_master_joints():
-    """获取主臂当前关节位置"""
-    master_status = master_arm.rm_get_current_arm_state()
-    if master_status[0] != 0:
-        print("无法获取主臂当前状态，错误码:", master_status[0])
-        return None
+# def master_state_func(data):
+#     global master_trajectory, master_time
+#     a = data.joint_status.to_dict()
+#     b = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
-    return master_status[1]['joint']
-
-def main_loop():
-    """主循环函数"""
-    global last_sent_joint, save_trajectory
+#     master_trajectory.append(a.copy())
+#     master_time.append(b)
     
-    try:
-        print("启动主从臂实时控制...")
+def slave_state_func(data):
+    global slave_trajectory, slave_time, save_images, image_time, pipeline1, pipeline2
+    # print('从臂状态回调')
+    a = data.joint_status.to_dict()
+    frames1 = pipeline1.wait_for_frames(timeout_ms=1000)
+    frames2 = pipeline2.wait_for_frames(timeout_ms=1000)
+    if not frames1 or not frames2:
+        print("无法获取颜色帧")
+    else:
+        color_frame1 = frames1.get_color_frame()
+        color_frame2 = frames2.get_color_frame()
+        color_image1 = np.asanyarray(color_frame1.get_data())
+        color_image2 = np.asanyarray(color_frame2.get_data())
+        b = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
         
-        # 确保从臂处于正常状态
+        slave_trajectory.append(a.copy())
+        slave_time.append(b)
+        save_images1.append(color_image1.copy())
+        save_images2.append(color_image2.copy())
+        image_time.append(b)
+               
+
+if __name__ == "__main__":
+    last_sent_joint = None
+    MIN_MOVEMENT_THRESHOLD = 0.05  # 最小运动阈值(弧度)
+    zhouqi = 6
+    master_trajectory = []
+    master_time = []
+    # 确定图像的输入分辨率与帧率
+    resolution_width = 640  # pixels
+    resolution_height = 480  # pixels
+    frame_rate = 60  # fps
+
+    # 注册数据流，并对其图像
+    rs_config = rs.config()
+    rs_config.enable_stream(rs.stream.color, resolution_width, resolution_height, rs.format.bgr8, frame_rate)
+    # check相机是不是进来了
+    connect_device = []
+    for d in rs.context().devices:
+        print('Found device: ',
+              d.get_info(rs.camera_info.name), ' ',
+              d.get_info(rs.camera_info.serial_number))
+        if d.get_info(rs.camera_info.name).lower() != 'platform camera':
+            connect_device.append(d.get_info(rs.camera_info.serial_number))
+
+    if len(connect_device) < 2:
+        print('Registrition needs two camera connected.But got one.')
+        exit()
+
+    # 确认相机并获取相机的内部参数
+    pipeline1 = rs.pipeline()
+    rs_config.enable_device(connect_device[0])
+    pipeline1.start(rs_config)
+
+    pipeline2 = rs.pipeline()
+    rs_config.enable_device(connect_device[1])
+    pipeline2.start(rs_config)
+
+    frames1 = pipeline1.wait_for_frames()
+    frames2 = pipeline2.wait_for_frames()
+
+    # 只在主线程创建
+    master_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
+    slave_arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
+    handle_master = master_arm.rm_create_robot_arm("192.168.110.119", 8080)
+    handle_slave = slave_arm.rm_create_robot_arm("192.168.110.118", 8080)
+    print('主臂创建成功', handle_master.id, handle_slave.id)
+
+    custom_slave = rm_udp_custom_config_t()
+    custom_slave.joint_speed = 1
+    custom_slave.lift_state = 0
+    custom_slave.expand_state = 0
+    custom_slave.arm_current_status = 1
+
+    # config_master = rm_realtime_push_config_t(zhouqi, True, 8089, 0, "192.168.110.55", custom)
+    # print('启动主臂UDP上报', master_arm.rm_set_realtime_push(config_master))
+    config_slave = rm_realtime_push_config_t(zhouqi, True, 8089, 0, "192.168.110.55", custom_slave)
+    print('启动从臂UDP上报', slave_arm.rm_set_realtime_push(config_slave))
+
+    try:
         slave_status = slave_arm.rm_get_current_arm_state()
-        print(f"从臂状态: {slave_status}")
         if slave_status[0] != 0:
             print("从臂未处于就绪状态，尝试复位...")
-            slave_arm.rm_reset_error()
+
+        master_status = master_arm.rm_get_current_arm_state()
+        if master_status[0] != 0:
+            print("主臂未处于就绪状态，尝试复位...")
         
-        # 打印主臂初始位置
-        master_joints = get_master_joints()
-        if master_joints:
-            print(f"主臂初始关节位置(弧度): {master_joints}")
+        arm_state_callback_slave = rm_realtime_arm_state_callback_ptr(slave_state_func)
+        slave_arm.rm_realtime_arm_state_call_back(arm_state_callback_slave)
         
-        # 主循环
+        # arm_state_callback_master = rm_realtime_arm_state_callback_ptr(master_state_func)
+        # master_arm.rm_realtime_arm_state_call_back(arm_state_callback_master)
+
         while True:
             # 记录循环开始时间
             start_time = time.time()
             
             # 获取主臂关节位置
-            master_joints = get_master_joints()
+            master_status = master_arm.rm_get_current_arm_state()
+            master_joints = master_status[1]['joint']
+            time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
             
             if master_joints:
-                print(f"主臂当前关节位置: {master_joints}")
                 
                 # 只有当移动距离超过阈值时才发送命令
                 if last_sent_joint is None or np.linalg.norm(np.array(master_joints) - np.array(last_sent_joint)) > MIN_MOVEMENT_THRESHOLD:
-                    print(f"发送关节位置到从臂: {master_joints}")
-                    
+
                     # 直接使用主臂关节位置控制从臂
                     result = slave_arm.rm_movej_follow(master_joints)
+                    # print(f"从臂移动到主臂位置: {master_joints}")
                     
                     if result != 0:
                         print(f"移动指令失败，错误码: {result}，尝试复位...")
-                        slave_arm.rm_reset_error()
                     else:
                         last_sent_joint = master_joints.copy()
-                        save_trajectory.append(master_joints)
-                else:
-                    print("变化量小于阈值，跳过")
+                        
+                        master_trajectory.append(master_joints.copy())
+                        master_time.append(time_stamp)
+                # else:
+                    # print("变化量小于阈值，跳过")
             else:
                 print('无法获取主臂数据') 
             
             # 计算已用时间
             elapsed_time = time.time() - start_time
             # 动态调整等待时间，确保每次循环大约0.1秒
-            sleep_time = max(0, 0.005 - elapsed_time)
+            sleep_time = max(0, 0.01 - elapsed_time)
             if sleep_time > 0:
                 time.sleep(sleep_time)
-                
-            # 打印实际循环时间
-            print(f"循环时间: {time.time() - start_time:.3f}秒")
 
     except KeyboardInterrupt:
-        with open(file_path_test, 'w', encoding='utf-8') as f:
-            for data in save_trajectory:
-                f.write(f"{data}\n")
-        print("保存轨迹数据到文件:", file_path_test)
-        master_arm.rm_movej([0, 0, 0, 0, 0, 0], 20, 0, 0, 1)
-        slave_arm.rm_movej([0, 0, 0, 0, 0, 0], 20, 0, 0, 1)
-        print("主从臂已复位")
-        master_arm.rm_delete_robot_arm()
-        slave_arm.rm_delete_robot_arm()
-        print("\n停止运行...")
+        print('检测到键盘中断，停止数据采集和控制...')
 
+    finally:
+        # config_end_master = rm_realtime_push_config_t(zhouqi, False, 8089, 0, "192.168.110.55", custom)
+        config_end_slave = rm_realtime_push_config_t(zhouqi, False, 8089, 0, "192.168.110.55", custom_slave)
+        # print("停止主臂数据采集", master_arm.rm_set_realtime_push(config_end_master))
+        master_arm.rm_movej([0, 0, 0, 0, 0, 0], 40, 0, 0, 1)
+        slave_arm.rm_movej([0, 0, 0, 0, 0, 0], 40, 0, 0, 1)
+        print("停止从臂数据采集", slave_arm.rm_set_realtime_push(config_end_slave))
+        print('从臂删除成功', slave_arm.rm_delete_robot_arm())
+        print('主臂删除成功', master_arm.rm_delete_robot_arm())
 
-if __name__ == "__main__":
-    main_loop()
+        pipeline1.stop()
+        pipeline2.stop()
+
+        master_pos = 'master_pos.csv'
+        slave_pos = 'slave_pos.csv'
+        slave_velocity = 'slave_velocity.csv'
+        data_path = os.path.join(os.getcwd(), 'data_image', 'a02')
+        master_pos_path = os.path.join(data_path, master_pos)
+        slave_pos_path = os.path.join(data_path, slave_pos)
+        slave_velocity_path = os.path.join(data_path, slave_velocity)
+        folder_path1 = os.path.join(data_path, 'images', 'cam1')
+        folder_path2 = os.path.join(data_path, 'images', 'cam2')
+        # 确保目录存在
+        os.makedirs(folder_path1, exist_ok=True)
+        os.makedirs(folder_path2, exist_ok=True)
+
+        print('data length:', len(master_trajectory), len(slave_trajectory), len(save_images1), len(save_images2))
+        print('time length:', len(master_time), len(slave_time), len(image_time))
+
+        # 保存主臂位置
+        with open(master_pos_path, 'w', encoding='utf-8') as f:
+            for ii, data in tqdm(enumerate(master_trajectory), total=len(master_trajectory), desc="Saving master pos"):
+                input_data = [master_time[ii]] + data
+                f.write(f"{input_data}\n")
+
+        # 保存从臂位置
+        with open(slave_pos_path, 'w', encoding='utf-8') as f:
+            for i0, data in tqdm(enumerate(slave_trajectory), total=len(slave_trajectory), desc="Saving slave pos"):
+                pos = data['joint_position']
+                input_data = [slave_time[i0].replace(':', '-').replace(' ', '_')] + pos
+                f.write(f"{input_data}\n")
+
+        # 保存从臂速度
+        with open(slave_velocity_path, 'w', encoding='utf-8') as f: 
+            for i1, data in tqdm(enumerate(slave_trajectory), total=len(slave_trajectory), desc="Saving slave vel"):
+                vel = data['joint_speed']
+                input_data = [slave_time[i1].replace(':', '-').replace(' ', '_')] + vel
+                f.write(f"{input_data}\n")
+
+        # 保存相机1图片
+        for i2, img in tqdm(enumerate(save_images1), total=len(save_images1), desc="Saving cam1 images"):
+            tmp_time = image_time[i2].replace(':', '-').replace(' ', '_')
+            image_path = os.path.join(folder_path1, f"{tmp_time}.png")
+            cv2.imwrite(image_path, img)
+
+        # 保存相机2图片
+        for i3, img3 in tqdm(enumerate(save_images2), total=len(save_images2), desc="Saving cam2 images"):
+            tmp_time3 = image_time[i3].replace(':', '-').replace(' ', '_')
+            image_path3 = os.path.join(folder_path2, f"{tmp_time3}.png")
+            cv2.imwrite(image_path3, img3)
+
+        # print('total time:', master_time[-1] - master_time[0], slave_time[-1] - slave_time[0], image_time[-1] - image_time[0])
+        print("程序结束")
+        RoboticArm.rm_destory()
